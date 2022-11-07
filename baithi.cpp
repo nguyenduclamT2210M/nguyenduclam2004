a7dc25707a5e8153e7b25290e06ff860e9bfe5d6
nguyenduclamT2210M <115681456+nguyenduclamT2210M@users.noreply.github.com>
2022-11-07 10:16:21 +0700

    Add files via upload

---
+++ baithi.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+	printf(" nhap so nguyen \n ");
+	
+	int arr[10];
+	for (int k=0; k<10;k++){
+		scanf("%d",&arr[k]);
+		
+	}
+	 printf("day dao nguoc\n");
+	for (int n=9 ; n>=0 ; n--){
+		printf("%d\n",arr[n]);
+     }
+
+}
